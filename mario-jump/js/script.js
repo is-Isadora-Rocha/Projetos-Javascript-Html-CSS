d555b4006e2cff72f682ffef5e3fce1ed0af77ea
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2023-06-28 21:45:15 -0300

    adição do banzaiBill

--- mario-jump/js/script.js
+++ mario-jump/js/script.js
@@ -1,6 +1,7 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
+const banzai = document.querySelector('.banzai');
 
 const jump = () => {
     mario.classList.add('jump');
@@ -16,6 +17,7 @@ const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+    const banzaiPosition = banzai.offsetLeft;
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
@@ -31,7 +33,24 @@ const loop = setInterval(() => {
 
         clearInterval(loop);
         clouds.style.animationPlayState = 'paused';
+        banzai.style.animationPlayState = 'paused';
 
+    }
+
+    if (banzaiPosition <= 100 && banzaiPosition > 0 && marioPosition < 80) {
+        banzai.style.animation = 'none';
+        banzai.style.left = `${banzaiPosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+        clouds.style.animationPlayState = 'paused';
+        pipe.style.animationPlayState = 'paused';
     }
 
 }, 10);
